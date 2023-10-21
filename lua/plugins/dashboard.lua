0e8a45abe15f2fe1414da00bd9df6627f862954a
esau-morais <esaumorais7@gmail.com>
2023-10-21 10:11:49 -0300

    feat: setup lazy vim

---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,56 @@
+local logo = [[
+ __        __   _                              _____                 
+ \ \      / /__| | ___ ___  _ __ ___   ___    | ____|___  __ _ _   _ 
+  \ \ /\ / / _ \ |/ __/ _ \| '_ ` _ \ / _ \   |  _| / __|/ _` | | | |
+   \ V  V /  __/ | (_| (_) | | | | | |  __/_  | |___\__ \ (_| | |_| |
+    \_/\_/ \___|_|\___\___/|_| |_| |_|\___( ) |_____|___/\__,_|\__,_|
+                                          |/                         
+]]
+logo = string.rep("\n", 8) .. logo .. "\n\n"
+
+return {
+  "nvimdev/dashboard-nvim",
+  opts = {
+    config = {
+      header = vim.split(logo, "\n"),
+      center = {
+        {
+          action = "Telescope oldfiles",
+          desc = " Recent files",
+          icon = " ",
+          key = "r",
+        },
+        {
+          action = "Telescope find_files",
+          desc = " Find file",
+          icon = " ",
+          key = "f",
+        },
+        {
+          action = "ene | startinsert",
+          desc = " New file",
+          icon = " ",
+          key = "n",
+        },
+        {
+          action = "Telescope live_grep",
+          desc = " Find text",
+          icon = " ",
+          key = "g",
+        },
+        {
+          action = 'lua require("persistence").load()',
+          desc = " Restore Session",
+          icon = " ",
+          key = "s",
+        },
+        {
+          action = "qa",
+          desc = " Quit",
+          icon = " ",
+          key = "q",
+        },
+      },
+    },
+  },
+}
