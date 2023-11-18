9217486ab9c9b95d769f2782414d8e2d5fef32ee
esau-morais <esaumorais7@gmail.com>
2023-11-18 16:05:26 -0300

    fix: remove no information available notification

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -63,4 +63,24 @@ return {
       },
     },
   },
+  {
+    "folke/noice.nvim",
+    opts = {
+      routes = {
+        {
+          filter = {
+            event = "notify",
+            find = "No information available",
+          },
+          opts = { skip = true },
+        },
+      },
+    },
+  },
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      timeout = 10000,
+    },
+  },
 }
